7f623e9c59d01b07529a1cbb779fa9b578d02c1e
LeonidVoroncov <voroncovleonid.45@mail.ru>
2021-04-19 10:53:51 +0500

    first commit

---
+++ ShootingRange/ShootingRange/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ShootingRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
