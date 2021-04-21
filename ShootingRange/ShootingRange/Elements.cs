8bc7cbab144ec002c0f321f144115efa29519459
LeonidVoroncov <voroncovleonid.45@mail.ru>
2021-04-21 06:57:48 +0500

    Second commit

---
+++ ShootingRange/ShootingRange/Elements.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Person
+{
+    public object Surname { get; set; }
+
+    public string DateBirth { get; set; }
+
+    public string Gender { get; set; }
+
+    public string Status { get; set; }
+
+    public bool License { get; set; }
+
+    public string TypeGun { get; set; }
+
+
+}
--- ShootingRange/ShootingRange/Program.cs
+++ ShootingRange/ShootingRange/Program.cs
@@ -6,7 +6,38 @@ namespace ShootingRange
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var array = new object[3] {
+                new Person
+                { 
+                    Surname = "Иванов",
+                    DateBirth = "17.08.2002",
+                    Gender = "мужской",
+                    Status = "Гражданский",
+                    License = false,
+                    TypeGun = "АК-47"
+                },
+                new Person
+                {
+                    Surname = "Патров",
+                    DateBirth = "17.08.2002",
+                    Gender = "мужской",
+                    Status = "Гражданский",
+                    License = true,
+                    TypeGun = "АК-47"
+                },
+                new Person
+                {
+                    Surname = "Сидоров",
+                    DateBirth = "17.08.2002",
+                    Gender = "мужской",
+                    Status = "Военный",
+                    TypeGun = "АК-47"
+                }
+            };
+            foreach(var element in array)
+            {
+                Console.WriteLine(element.License);
+            }           
             Console.ReadKey();
         }
     }
